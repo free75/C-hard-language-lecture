aed7e73f8c9eee6c593bc86ab9f67d5141f65e4b
free75 <hyunho2st@gmail.com>
2026-04-16 19:45:10 +0900

    (2026 4. 16) 시간 시스템

--- program/source.c
+++ program/source.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+#define _CRT_SECURE_NO_WARNING
+
+#define SIZE 10000
+
+void load(const char * file_name)
+{
+	FILE* file = fopen(file_name, "r");
+
+	char buffer[SIZE] = { 0, };
+
+	// 첫 번째 매개 변수 : 읽을 데이터를 저장할 메모리 버퍼의 포인터 변수
+	// 두 번째 매개 변수 : 각 데이터 항목의 크기
+	// 세 번째 매개 변수 : 데이터를 읽어올 항목의 수
+	// 네 번째 매개 변수 : 데이터를 읽어올 파일의 포인터 변수
+
+	fread(buffer, sizeof(char), SIZE, file);
+
+	printf("%s", buffer);
+
+	fclose(file);
+}
+
+
 int main()
 {
 #pragma region 파일 입출력
@@ -18,7 +41,9 @@ int main()
 
 #pragma endregion
 
-	FILE* file = fopen("test.txt", "w"); // fopen : 말 그대로 새로운 파일을 만들게 도와주는 명령어입니다.
+	load("poketmon.txt");
+
+	FILE* file = fopen("poketmon.txt", "w"); // fopen : 말 그대로 새로운 파일을 만들게 도와주는 명령어입니다.
 
 #pragma region 파일에 원하는 문자 출력하는 방법
 
@@ -32,25 +57,43 @@ int main()
 
 # pragma endregion
 
-	fputs("안녕하세요", file); // fputs: 만든 파일 안에 입력할 문자 혹은 숫자들을 적도록 만들어 주는 명령어입니다.
+	fputs("%d", file); // fputs: 만든 파일 안에 입력할 문자 혹은 숫자들을 적도록 만들어 주는 명령어입니다.
+
+	fgetc(file);
+
+	fopen("poketmon", "r");
+
+	int count = 0;
+	
+	while 
+
+	
 
 	fclose(file);
 
-#pragma endregion
-
-#pragma region
+	
 
 
 
 #pragma endregion
 
-#pragma region
+#pragma region 
 
-
+#pragma region fread 사용방법
+	// fread를 그냥 사용한다면 메모리의 크기가 작아서 실행이 불가능하게 되는데
+	// 이걸 방지하기 위해 fread를 사용하기 전에 #define SIZE를 사용하여
+	// 메모리 크기를 크게 만들어 준다음 원하는 변수 하나를 선언하여 선언했던
+	// SIZE를 통해 메모리 크기를 동기화 시켜줍니다.
+	// 예시로 buffer라는 함수를 선언했을때 이걸 SIZE와 결합하여
+	// #define SIZE 1000과 char buffer[SIZE] = { 0, }; 로 메모리 크기를
+	// 1000으로 만들어 준다음, 그 후엔 메모리 크기가 부족하여
+	// 사용하지 못 했던 fread를 메모리 크기가 1000인 buffer와 합치면
+	// fread안에 먼저 buffer를 선언해준 다음 메모리 크기를 사용하기 위해
+	// sizeof와 buffer의 자료형인 char을 사용하여 메모리 크기까지 넣습니다.
+	// 후엔 출력문과 비슷하게 SIZE와 정확히 파일 안에 집어넣은 파일의 이름을
+	// 쓴다면 실행이 가능하게 됩니다.
 
 #pragma endregion
-
-#pragma region
 
 
 
